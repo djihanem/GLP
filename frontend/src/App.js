71fbe2f45593a89822ba4df28d5c28e121a9aeff
bassmala-belguidoum <129202994+bassmala-belguidoum@users.noreply.github.com>
2024-01-26 16:00:19 +0100

    signup and login client views

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ import Profile from "./component/Profile";
 import EditProfile from "./component/EditProfile";
 import AppointmentSection from "./component/AppointementSection";
 import SignUpAvocat from "./component/avocat/SignUpAvocat";
+import LoginUser from "./component/user/userlogin";
 
 
 function App() {
@@ -24,6 +25,7 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/lawyer" element={<Lawyer />} />
           <Route path="/avocat" element={<SignUpAvocat/>}/>
+          <Route path="/LoginAvocat" element={<loginAvocat/>}/>
           <Route path="/user" element={<User />} />
           <Route path="/footer" element={<Footer />} />
           <Route path="/profil/:idlawyer" element={<Profile />} />
